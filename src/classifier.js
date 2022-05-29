ac64c892e0d79952e2b77dea09bed55e69e2db30
Achionyinye <achionyinyegiftbarbie@gmail.com>
2022-05-29 11:48:25 +0100

    add week-2 to my repo

--- src/classifier.js
+++ src/classifier.js
@@ -1,292 +1,180 @@
-// function classifier(input) {
-//     const groupArr = {
+// function classifier(input) {}
 
-//     }
-
-//     let group
-//     let noOfGroups = input.length
-//     //console.log(noOfGroups)
-//     for (let i = 0; i < input.length; i++) {
-//       let arrAge = input[i]["dob"];
-//       console.log(arrAge)
-//       let dateOfBirth = arrAge.slice(0, 4)
-//       console.log(dateOfBirth)
-//       let currentYear = 2019
-//       let calcAge = Number(currentYear - dateOfBirth)
-//       //calcAge.split(',')
-//      console.log(calcAge)
-//       let smallestAge = calcAge
-//       if(smallestAge < calcAge)
-//     //  for (let j = 0; j < calcAge.length; j++) {
-//     //     console.log(calcAge)
-//     //  }
-//       //console.log(typeof calcAge)
-//       // let smallestAge = calcAge.sort(function(a, b){return b - a});
-//       // console.log(smallestAge)
-//       //calcAge.map(x => x - y);
-//       // if (calcAge.length > 3){
-//       //   group ++
-//       // }
-//       // let smallestAge = Math.max(calcAge)
-//       // console.log(smallestAge)
-
-//       let smallestAge;
-//       if(smallestAge < calcAge[0]){
-//           console.log(calcAge)
-//         console.log(smallestAge)
-//         smallestAge = calcAge
-//         console.log(smallestAge)
-//       }
-
-//     } return noOfGroups
-//   } console.log(classifier(
-//     [
-//       {
-//         name: 'Hendrick',
-//         dob: '1853-07-18T00:00:00.000Z',
-//         regNo: '041',
-//       },
-//       {
-//         name: 'Albert',
-//         dob: '1910-03-14T00:00:00.000Z',
-//         regNo: '033',
-//       },
-//       {
-//         name: 'Marie',
-//         dob: '1953-11-07T00:00:00.000Z',
-//         regNo: '024',
-//       },
-//       {
-//         name: 'Neils',
-//         dob: '1853-10-07T00:00:00.000Z',
-//         regNo: '02',
-//       },
-//       {
-//         name: 'Max',
-//         dob: '1853-04-23T00:00:00.000Z',
-//         regNo: '014',
-//       },
-//       {
-//         name: 'Erwin',
-//         dob: '1854-08-12T00:00:00.000Z',
-//         regNo: '09',
-//       },
-//       {
-//         name: 'Auguste',
-//         dob: '1854-01-28T00:00:00.000Z',
-//         regNo: '08',
-//       },
-//       {
-//         name: 'Karl',
-//         dob: '1852-12-05T00:00:00.000Z',
-//         regNo: '120',
-//       },
-//       {
-//         name: 'Louis', //
-//         dob: '1852-08-15T00:00:00.000Z',
-//         regNo: '022',
-//       },
-//       {
-//         name: 'Arthur',
-//         dob: '1892-09-10T00:00:00.000Z',
-//         regNo: '321',
-//       },
-//       {
-//         name: 'Paul',
-//         dob: '1902-08-08T00:00:00.000Z',
-//         regNo: '055',
-//       },
-//       {
-//         name: 'William',
-//         dob: '1890-03-31T00:00:00.000Z',
-//         regNo: '013',
-//       },
-//       {
-//         name: 'Owen',
-//         dob: '1853-04-26T00:00:00.000Z',
-//         regNo: '052',
-//       },
-//       {
-//         name: 'Martin',
-//         dob: '1854-02-15T00:00:00.000Z',
-//         regNo: '063',
-//       },
-//       {
-//         name: 'Guye',
-//         dob: '1854-10-15T00:00:00.000Z',
-//         regNo: '084',
-//       },
-//       {
-//         name: 'Charles',
-//         dob: '1954-02-14T00:00:00.000Z',
-//         regNo: '091',
-//       },
-//     ]
-//   ));
 // export default classifier;
 
 
-function classifier(input) {
+// input contains and array of objects
+//  iterate through the array
+// for each, check if the object has a property called "name" and "age"
+
+
+
+/**
+ * This is the entry point to the program
+ * Question 1 - Classifier
+ *
+ */
+ function classifier(input) {
     // return if input is not an array or array length is less than 1
     if (!Array.isArray(input)) {
-        throw Error;
+      throw Error;
     }
     if (!input.length) {
-        return { noOfGroups: 0 };
+      return { noOfGroups: 0 };
     }
-
+  
     const newArr = [...input];
-
+  
     // calculate age of students
     const modifiedArray = newArr.map((element) => ({
-        name: element.name,
-        age: calcAge(element.dob),
-        regNo: element.regNo,
-        dob: element.dob,
+      name: element.name,
+      age: calcAge(element.dob),
+      regNo: element.regNo,
+      dob: element.dob,
     }));
-
+  
     // sort array by age
     const sortedArray = modifiedArray.sort(function (a, b) {
-        return a.age - b.age;
+      return a.age - b.age;
     });
-
+  
     //initialize 1st group with the first student in the sorted array
     let group = [sortedArray[0]];
     let studentGroup = [];
-
+  
     // sort group by age difference and group length
     for (let i = 1; i < modifiedArray.length; i++) {
-        if (sortedArray[i].age - group[0].age <= 5 && group.length <= 2) {
-            group.push(modifiedArray[i]);
-        } else {
-            studentGroup.push(group);
-            group = [];
-            group.push(modifiedArray[i]);
-        }
+      if (sortedArray[i].age - group[0].age <= 5 && group.length <= 2) {
+        group.push(modifiedArray[i]);
+      } else {
+        studentGroup.push(group);
+        group = [];
+        group.push(modifiedArray[i]);
+      }
     }
-
+  
     // last group
     if (group.length) {
-        studentGroup.push(group);
+      studentGroup.push(group);
     }
-
+  
     // set noOfGroups key
     let output = {};
     output.noOfGroups = studentGroup.length;
-
+  
     // format groups based on output requirement
     const groupOutput = studentGroup.map(function (group) {
-        return {
-            members: group.map((el) => ({
-                name: el.name,
-                age: el.age,
-                dob: el.dob,
-                regNo: el.regNo,
-            })),
-            oldest: group[group.length - 1].age,
-            sum: group.reduce((acc, el) => {
-                return acc + el.age;
-            }, 0),
-            regNos: group.map(el => (Number(el.regNo))).sort(function (a, b) {
-                return a - b
-            })
-        };
+      return {
+        members: group.map((el) => ({
+          name: el.name,
+          age: el.age,
+          dob: el.dob,
+          regNo: el.regNo,
+        })),
+        oldest: group[group.length - 1].age,
+        sum: group.reduce((acc, el) => {
+          return acc + el.age;
+        }, 0),
+        regNos: group.map(el => (Number(el.regNo))).sort(function(a, b) {
+          return a - b
+        })
+      };
     });
-
+  
     // set output key for each group
     groupOutput.forEach((group, idx) => {
-        let currentGroup = `group${idx + 1}`;
-        output = { ...output, [currentGroup]: group };
+      let currentGroup = `group${idx + 1}`;
+      output = { ...output, [currentGroup]: group };
     });
-
+  
     return output;
-}
-
-// function to calculate age
-const calcAge = (year) => {
+  }
+  
+  // function to calculate age
+  const calcAge = (year) => {
     const date = new Date(year);
     return new Date(2019, 0, 1).getFullYear() - new Date(year).getFullYear()
-};
-console.log(classifier([
+  };
+  console.log(classifier([
     {
-        name: 'Hendrick',
-        dob: '1853-07-18T00:00:00.000Z',
-        regNo: '041',
+      name: 'Hendrick',
+      dob: '1853-07-18T00:00:00.000Z',
+      regNo: '041',
     },
     {
-        name: 'Albert',
-        dob: '1879-03-14T00:00:00.000Z',
-        regNo: '033',
+      name: 'Albert',
+      dob: '1879-03-14T00:00:00.000Z',
+      regNo: '033',
     },
     {
-        name: 'Marie',
-        dob: '1867-11-07T00:00:00.000Z',
-        regNo: '024',
+      name: 'Marie',
+      dob: '1867-11-07T00:00:00.000Z',
+      regNo: '024',
     },
     {
-        name: 'Neils',
-        dob: '1885-10-07T00:00:00.000Z',
-        regNo: '02',
+      name: 'Neils',
+      dob: '1885-10-07T00:00:00.000Z',
+      regNo: '02',
     },
     {
-        name: 'Max',
-        dob: '1858-04-23T00:00:00.000Z',
-        regNo: '014',
+      name: 'Max',
+      dob: '1858-04-23T00:00:00.000Z',
+      regNo: '014',
     },
     {
-        name: 'Erwin',
-        dob: '1887-08-12T00:00:00.000Z',
-        regNo: '09',
+      name: 'Erwin',
+      dob: '1887-08-12T00:00:00.000Z',
+      regNo: '09',
     },
     {
-        name: 'Auguste',
-        dob: '1884-01-28T00:00:00.000Z',
-        regNo: '08',
+      name: 'Auguste',
+      dob: '1884-01-28T00:00:00.000Z',
+      regNo: '08',
     },
     {
-        name: 'Karl',
-        dob: '1901-12-05T00:00:00.000Z',
-        regNo: '120',
+      name: 'Karl',
+      dob: '1901-12-05T00:00:00.000Z',
+      regNo: '120',
     },
     {
-        name: 'Louis', //
-        dob: '1892-08-15T00:00:00.000Z',
-        regNo: '022',
+      name: 'Louis', //
+      dob: '1892-08-15T00:00:00.000Z',
+      regNo: '022',
     },
     {
-        name: 'Arthur',
-        dob: '1892-09-10T00:00:00.000Z',
-        regNo: '321',
+      name: 'Arthur',
+      dob: '1892-09-10T00:00:00.000Z',
+      regNo: '321',
     },
     {
-        name: 'Paul',
-        dob: '1902-08-08T00:00:00.000Z',
-        regNo: '055',
+      name: 'Paul',
+      dob: '1902-08-08T00:00:00.000Z',
+      regNo: '055',
     },
     {
-        name: 'William',
-        dob: '1890-03-31T00:00:00.000Z',
-        regNo: '013',
+      name: 'William',
+      dob: '1890-03-31T00:00:00.000Z',
+      regNo: '013',
     },
     {
-        name: 'Owen',
-        dob: '1879-04-26T00:00:00.000Z',
-        regNo: '052',
+      name: 'Owen',
+      dob: '1879-04-26T00:00:00.000Z',
+      regNo: '052',
     },
     {
-        name: 'Martin',
-        dob: '1871-02-15T00:00:00.000Z',
-        regNo: '063',
+      name: 'Martin',
+      dob: '1871-02-15T00:00:00.000Z',
+      regNo: '063',
     },
     {
-        name: 'Guye',
-        dob: '1866-10-15T00:00:00.000Z',
-        regNo: '084',
+      name: 'Guye',
+      dob: '1866-10-15T00:00:00.000Z',
+      regNo: '084',
     },
     {
-        name: 'Charles',
-        dob: '1868-02-14T00:00:00.000Z',
-        regNo: '091',
+      name: 'Charles',
+      dob: '1868-02-14T00:00:00.000Z',
+      regNo: '091',
     },
-]))
-module.exports = classifier;
+  ]))
+  module.exports = classifier;
--- src/duplicate-transactions.js
+++ src/duplicate-transactions.js
@@ -1,406 +1,98 @@
-// const sourceAccount = require('sourceAccount');
-// function findDuplicateTransactions(transactions) { }
-//create an array that takes in the individual metod
-//loop through the array and compare the inputs 
-//check time for each reciept
-// <=1 minuite, push in same array
-//read on string interpolation or computed properties
+// function findDuplicateTransactions(transactions) {}
 
-//loop through input
-//if (source ===
+// export default findDuplicateTransactions;
 
 
-// ---------------------------------------------------
+function findDuplicatetransactions(transactions) {
 
-let input = [
-    {
-        id: 201,
-        sourceAccount: 'company_x',
-        targetAccount: 'my_account',
-        amount: 10000,
-        category: 'pension_benefits',
-        time: '2018-02-25T08:00:00.000Z',
-      },
-      {
-        id: 1,
-        sourceAccount: 'company_x',
-        targetAccount: 'my_account',
-        amount: 10000,
-        category: 'salary',
-        time: '2018-02-25T08:00:00.000Z',
-      },
-      {
-        id: 101,
-        sourceAccount: 'company_x',
-        targetAccount: 'my_account',
-        amount: 240,
-        category: 'salary',
-        time: '2018-02-25T08:00:30.000Z',
-      },
-      {
-        id: 2,
-        sourceAccount: 'my_account',
-        targetAccount: 'coffee_shop',
-        amount: -50,
-        category: 'eating_out',
-        time: '2018-03-01T12:34:00.000Z',
-      },
-      {
-        id: 3,
-        sourceAccount: 'my_account',
-        targetAccount: 'supermarket',
-        amount: -1000,
-        category: 'groceries',
-        time: '2018-03-01T17:28:32.000Z',
-      },
-      {
-        id: 4,
-        sourceAccount: 'my_account',
-        targetAccount: 'cinema',
-        amount: -330,
-        category: 'other',
-        time: '2018-03-01T20:10:15.000Z',
-      },
-      {
-        id: 6,
-        sourceAccount: 'my_account',
-        targetAccount: 'internet_shop',
-        amount: -250,
-        category: 'other',
-        time: '2018-03-01T22:16:40.000Z',
-      },
-      {
-        id: 102,
-        sourceAccount: 'my_account',
-        targetAccount: 'internet_shop',
-        amount: -250,
-        category: 'other',
-        time: '2018-03-01T22:16:50.000Z',
-      },
-      {
-        id: 5,
-        sourceAccount: 'my_account',
-        targetAccount: 'coffee_shop',
-        amount: -50,
-        category: 'eating_out',
-        time: '2018-03-02T09:25:20.000Z',
-      },
-      {
-        id: 7,
-        sourceAccount: 'my_account',
-        targetAccount: 'supermarket',
-        amount: -160,
-        category: 'groceries',
-        time: '2018-03-02T13:14:00.000Z',
-      },
-      {
-        id: 8,
-        sourceAccount: 'my_account',
-        targetAccount: 'restaurant',
-        amount: -670,
-        category: 'eating_out',
-        time: '2018-03-02T18:54:45.000Z',
-      },
-      {
-        id: 9,
-        sourceAccount: 'my_account',
-        targetAccount: 'coffee_shop',
-        amount: -50,
-        category: 'eating_out',
-        time: '2018-03-04T07:14:20.000Z',
-      },
-      {
-        id: 10,
-        sourceAccount: 'my_account',
-        targetAccount: 'fitness_club',
-        amount: -560,
-        category: 'other',
-        time: '2018-03-04T12:54:10.000Z',
-      },
-      {
-        id: 11,
-        sourceAccount: 'my_account',
-        targetAccount: 'supermarket',
-        amount: -1540,
-        category: 'groceries',
-        time: '2018-03-05T16:24:31.000Z',
-      },
-      {
-        id: 12,
-        sourceAccount: 'my_account',
-        targetAccount: 'bowling_place',
-        amount: -600,
-        category: 'other',
-        time: '2018-03-05T21:12:10.000Z',
-      },
-      {
-        id: 16,
-        sourceAccount: 'company_x',
-        targetAccount: 'my_account',
-        amount: 10000,
-        category: 'salary',
-        time: '2018-03-25T08:10:00.000Z',
-      },
-      {
-        id: 13,
-        sourceAccount: 'my_account',
-        targetAccount: 'coffee_shop',
-        amount: -50,
-        category: 'eating_out',
-        time: '2018-04-01T10:24:00.000Z',
-      },
-      {
-        id: 14,
-        sourceAccount: 'my_account',
-        targetAccount: 'coffee_shop',
-        amount: -50,
-        category: 'eating_out',
-        time: '2018-04-01T10:24:40.000Z',
-      },
-      {
-        id: 15,
-        sourceAccount: 'my_account',
-        targetAccount: 'coffee_shop',
-        amount: -50,
-        category: 'eating_out',
-        time: '2018-04-01T10:25:10.000Z',
-      },
-      {
-        id: 17,
-        sourceAccount: 'my_account',
-        targetAccount: 'supermarket',
-        amount: -1870,
-        category: 'groceries',
-        time: '2018-04-05T10:24:30.000Z',
-      },
-      {
-        id: 18,
-        sourceAccount: 'my_account',
-        targetAccount: 'cinema',
-        amount: -580,
-        category: 'other',
-        time: '2018-04-05T20:01:18.000Z',
-      },
-      {
-        id: 19,
-        sourceAccount: 'my_account',
-        targetAccount: 'coffee_shop',
-        amount: -90,
-        category: 'eating_out',
-        time: '2018-04-07T09:54:21.000Z',
-      },
-      {
-        id: 20,
-        sourceAccount: 'my_account',
-        targetAccount: 'internet_shop',
-        amount: -1650,
-        category: 'other',
-        time: '2018-04-08T21:36:41.000Z',
-      },
-      {
-        id: 21,
-        sourceAccount: 'my_account',
-        targetAccount: 'supermarket',
-        amount: -1690,
-        category: 'groceries',
-        time: '2018-04-10T18:14:10.000Z',
-      },
-      {
-        id: 23,
-        sourceAccount: 'my_account',
-        targetAccount: 'coffee_shop',
-        amount: -70,
-        category: 'eating_out',
-        time: '2018-04-15T09:12:20.000Z',
-      },
-      {
-        id: 22,
-        sourceAccount: 'my_account',
-        targetAccount: 'restaurant',
-        amount: -970,
-        category: 'eating_out',
-        time: '2018-04-17T19:52:46.000Z',
-      },
-      {
-        id: 25,
-        sourceAccount: 'my_account',
-        targetAccount: 'supermarket',
-        amount: -850,
-        category: 'groceries',
-        time: '2018-04-20T18:51:31.000Z',
-      },
-      {
-        id: 24,
-        sourceAccount: 'my_account',
-        targetAccount: 'fitness_club',
-        amount: -610,
-        category: 'other',
-        time: '2018-04-22T11:54:10.000Z',
-      },
-      {
-        id: 26,
-        sourceAccount: 'my_account',
-        targetAccount: 'cinema',
-        amount: -450,
-        category: 'other',
-        time: '2018-04-23T19:13:10.000Z',
-      },
-      {
-        id: 27,
-        sourceAccount: 'company_x',
-        targetAccount: 'my_account',
-        amount: 10000,
-        category: 'salary',
-        time: '2018-04-25T08:00:00.000Z',
-      },
-      {
-        id: 28,
-        sourceAccount: 'my_account',
-        targetAccount: 'supermarket',
-        amount: -1870,
-        category: 'groceries',
-        time: '2018-05-05T10:24:30.000Z',
-      },
-      {
-        id: 29,
-        sourceAccount: 'my_account',
-        targetAccount: 'cinema',
-        amount: -580,
-        category: 'other',
-        time: '2018-05-05T20:01:18.000Z',
-      },
-      {
-        id: 30,
-        sourceAccount: 'my_account',
-        targetAccount: 'coffee_shop',
-        amount: -90,
-        category: 'eating_out',
-        time: '2018-05-07T09:54:21.000Z',
-      },
-      {
-        id: 31,
-        sourceAccount: 'my_account',
-        targetAccount: 'coffee_shop',
-        amount: -90,
-        category: 'eating_out',
-        time: '2018-05-07T09:55:10.000Z',
-      },
-      {
-        id: 32,
-        sourceAccount: 'my_account',
-        targetAccount: 'coffee_shop',
-        amount: -90,
-        category: 'eating_out',
-        time: '2018-05-07T09:56:09.000Z',
-      },
-      {
-        id: 33,
-        sourceAccount: 'my_account',
-        targetAccount: 'coffee_shop',
-        amount: -90,
-        category: 'eating_out',
-        time: '2018-05-07T09:57:05.000Z',
-      },
-      {
-        id: 35,
-        sourceAccount: 'my_account',
-        targetAccount: 'coffee_shop',
-        amount: -90,
-        category: 'eating_out',
-        time: '2018-05-07T09:58:06.000Z',
-      },
-      {
-        id: 36,
-        sourceAccount: 'my_account',
-        targetAccount: 'internet_shop',
-        amount: -1650,
-        category: 'other',
-        time: '2018-05-08T21:36:41.000Z',
-      },
-      {
-        id: 37,
-        sourceAccount: 'my_account',
-        targetAccount: 'supermarket',
-        amount: -1690,
-        category: 'groceries',
-        time: '2018-05-10T18:14:10.000Z',
-      },
-      {
-        id: 39,
-        sourceAccount: 'my_account',
-        targetAccount: 'coffee_shop',
-        amount: -70,
-        category: 'eating_out',
-        time: '2018-05-15T09:12:20.000Z',
-      },
-      {
-        id: 38,
-        sourceAccount: 'my_account',
-        targetAccount: 'restaurant',
-        amount: -970,
-        category: 'eating_out',
-        time: '2018-05-17T19:52:46.000Z',
-      },
-      {
-        id: 41,
-        sourceAccount: 'my_account',
-        targetAccount: 'supermarket',
-        amount: -850,
-        category: 'groceries',
-        time: '2018-05-20T18:51:31.000Z',
-      },
-      {
-        id: 40,
-        sourceAccount: 'my_account',
-        targetAccount: 'fitness_club',
-        amount: -610,
-        category: 'other',
-        time: '2018-05-22T11:54:10.000Z',
-      },
-      {
-        id: 42,
-        sourceAccount: 'my_account',
-        targetAccount: 'cinema',
-        amount: -450,
-        category: 'other',
-        time: '2018-05-23T19:13:10.000Z',
-      },
-    ];
+  // checking if transaction array is empty 
+  if(transactions === null || transactions === ""){
+      // if transcation array is empty then throw an error exception
+      throw new Error("Invalid input");
+  }
+
+  // use the spread operator to create a new array and store in trasactionArray
+  let transactionArray= [...transactions];
+
+  // checking if transaction array is of type object
+  if(typeof transactionArray !== "object"){
+      // if transaction array is not an object then throw a new error exception
+      throw new Error ("Invalid Input");
+  }
+
+  // sorting transaction array based on transaction time which was converted to a date object
+  transactionArray.sort((a, b) => new Date(a.time) - new Date(b.time));
+  
+  let duplicates = []
+  let match = []
+  let src, dst;
+
+  // while the sorted transaction array's length is greater than 1 continue loop
+  while(transactionArray.length > 1){
+  
+    src = 0
+    dst = 1
+    match = [src]
+
+    // the date object of transaction time minus
+    // the date object of transaction time of src 
+    // the src is the base transaction time or lowest transaction time
+    while ((new Date(transactionArray[dst].time) - new Date(transactionArray[src].time)) <= 60000 && src < transactionArray.length -1){
+      
+      // if the difference is less than 60 seconds then the src and dst are the same
+      if(compare_equal(transactionArray[src], transactionArray[dst])){
+        // push to match array 
+          match.push(dst)
+          src = dst
+      }
+
+      // increment dst
+      dst += 1
+
+      // if dst is greater than the length of the transaction array then break the loop **'
+      if (dst === transactionArray.length){
+        break
+      }
+    }
+
     
+    // if match array length is greater than 1 then there are duplicates
+    if (match.length > 1){
+      let temp = []
 
-var duplicateSourceAccounts = input.reduce(function (acc, el, i, arr) {
-    if ((arr.indexOf(el.sourceAccount) !== i) && (acc.indexOf(el.sourceAccount) < 0)) acc.push(el.sourceAccount); return acc;
-}, []).sort();
+      // for each element in match array (as long as the match length is graeter than 1)
+      while(match.length){
 
-var duplicateTargetAccounts = input.reduce(function (acc, el, i, arr) {
-    if ((arr.indexOf(el.targetAccount) !== i) && (acc.indexOf(el.targetAccount) < 0)) acc.push(el.targetAccount); return acc;
-}, []).sort();
+          // push the element to temp array
+        let item = transactionArray.splice(match.pop(), 1)
 
-var duplicateAmounts = input.reduce(function (acc, el, i, arr) {
-    if ((arr.indexOf(el.amount) !== i) && (acc.indexOf(el.amount) < 0)) acc.push(el.amount); return acc;
-}, []).sort();
+        // push the temp array to duplicates array
+        temp.unshift(item[0])
+      }
 
-var duplicateCategories = input.reduce(function (acc, el, i, arr) {
-    if ((arr.indexOf(el.category) !== i) && (acc.indexOf(el.category) < 0)) acc.push(el.category); return acc;
-}, []).sort();
+      //
+      duplicates.push(temp)
+      match = []
+    } else{
+      transactionArray.shift()
+    }
 
-let output = [];
-let i = 0;
-duplicateTargetAccounts.forEach(targetAccount => {
-    output[i] = []
-    input.forEach(detail => {
-        if (targetAccount == detail.targetAccount && detail.amount == duplicateAmounts[i] && detail.category == duplicateCategories[i]) {
-            output[i].push(detail)
-        }
-    })
+    if (transactionArray.length == 1){
+      break;
+    }  
+    
+  }
+  return duplicates
+}
 
-    output[i] = output[i].sort((a, b) => {
-        return a.time.split('T')[1].split(':')[1] - b.time.split('T')[1].split(':')[1]
-    })
 
-    output[i] = output[i].filter((detail) => detail.time.split('T')[1].split(':')[1] - output[i][0].time.split('T')[1].split(':')[1] < 2);
+const compare_equal = (t1,t2) => {
+  if (t1.sourceAccount === t2.sourceAccount && t1.targetAccount === t2.targetAccount && t1.amount === t2.amount && t1.category === t2.category){
+    return true;
+  }else{
+    return false;
+  }
+}
 
-    i++;
-});
 
-console.log(input);
+export default findDuplicatetransactions;
